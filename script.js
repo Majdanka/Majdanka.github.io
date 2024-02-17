a03953f4ac66701fb42b437d5bb2ef04a8409084
Majdanka <majdan_k@onet.pl>
2024-02-17 02:11:20 +0100

    Idk why I did it pls don't judge

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-document.getElementById('y').addEventListener('click', function() {
-  console.log('y');
+const urls = [
+  'https://media.tenor.com/yk8nhx3fZGYAAAAj/sebas-bleizeffer.gif',
+  'https://assets.stickpng.com/images/5eaaab8cad89b60004c4c7b9.png'
+];
+
+document.querySelector('img').src = urls[1];
+
+var counter = 0;
+
+document.getElementById('y').onclick = function() { SheSaidYes(); };
+
+document.getElementById('n').addEventListener('click', function() {
+    WhyWouldYouDoThat(counter);
 });
+
+function WhyWouldYouDoThat(c) {
+  switch (c) {
+    case 0:
+        document.getElementById('n').innerText = 'Are you sure?';
+        document.getElementById('y').style.fontWeight = 900;
+        break;
+    case 1:
+        document.getElementById('n').innerText = 'Really really sure?';
+        document.getElementById('y').style.fontSize = '2em';
+        break;
+    case 2:
+        document.getElementById('n').innerText = 'Last chance...';
+        document.getElementById('y').style.fontSize = '3em';
+        document.getElementById('y').style.textDecoration = 'underline';
+        break;
+    case 3:
+        document.getElementById('n').innerText = 'Yes';
+        break;
+    case 4:
+        SheSaidYes();
+  }
+    counter++;
+}
+
+function SheSaidYes() {
+    document.querySelector('img').src = urls[0];
+    document.querySelectorAll('div')[1].style.display = 'none'; 
+    document.querySelector('h1').innerText = 'OMGGGGG!';
+}
